149befa72c24daaac17ab63079890df2ca36c164
Flávio <flaviowander@msn.com>
2015-06-20 13:47:04 -0300

    .

--- INS_insercao.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "INS_insercao.h"
-#include "TRE_tree.h"
-
-void INS_insert(tree *x,int dado)
-{
-	//insere o valor "dado" na árvore "x";
-}
--- INS_insercao.h
+++
@@ -1,6 +0,0 @@
-#ifndef INS_INSERCAO_H
-#define	INS_INSERCAO_H
-
-void INS_insert(tree *x,int dado);
-
-#endif	/* INS_INSERCAO_H */
